e9cc4ead369d0a1e5e5b1505e05186bae1895023
Vipul Basapati <basapativipulkumar@gmail.com>
2018-06-16 12:42:54 +0530

    grabbed the value using ref from StorePicker and pushed the new state using router history

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,27 @@ import React from 'react';
 import { getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    handleSubmit = event => {
+        event.preventDefault();
+        
+        const storeName = this.myInput.value.value;
+
+        this.props.history.push(`/store/${ storeName }`);
+    }
+
     render() {
         return (
-            <form action="" className="store-selector">
+            <form action="" className="store-selector" onSubmit={this.handleSubmit}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } />
+                <input 
+                    type="text" 
+                    ref={this.myInput}
+                    required 
+                    placeholder="Store Name" 
+                    defaultValue={ getFunName() } 
+                />
                 <button type="submit">Visit Store -></button>
             </form>
         );
